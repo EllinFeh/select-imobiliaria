56438540c0d19cde69b6729cbed633f19b811a8d
Elison Felipe <elisonfelipe16@hotmail.com>
2025-04-06 17:30:35 -0300

    jangaa

--- front-end/src/app/janga-garden/page.jsx
+++ front-end/src/app/janga-garden/page.jsx
@@ -8,7 +8,7 @@ export default function imovelOne() {
             <NavbarDois></NavbarDois>
             <div className="bg-[url('/images/imoveltwo.png')] bg-cover bg-center flex h-screen justify-center items-center">
                 <div className='justify-center flex items-center flex-col'>
-                    <img className='w-[350px] mt-24' src="./images/jangalogo.png" alt="" />
+                    <img className='w-[350px] mt-24' src="./images/logojangaa.png" alt="" />
                     <Card id_imovel="VEM_31709" />
                 </div>
             </div>
